9caea293b214c99373dcfe7233bd7ab3ac4800ea
HvarZ <hvarzahar@gmail.com>
2021-11-02 18:11:00 +0300

    add implementations of functions: create, add, delete

--- include/telephone_info.h
+++ include/telephone_info.h
@@ -2,6 +2,8 @@
 #define INDIVIDUAL_TASK_1_TELEPHONE_INFO_H
 
 #include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
 
 typedef struct TelephoneInfo {
     unsigned int networkCode_;
@@ -15,16 +17,16 @@ typedef struct TelephoneBase {
     telephone_t* telephones;
 } base_t;
 
-void createBase(base_t* base);
+void CreateBase(base_t* base);
 
-void add(base_t* base, const unsigned int networkCode,
-         const unsigned int number, const char* nameOwner);
+void Add(base_t* base, unsigned int networkCode,
+         unsigned int number, const char* nameOwner);
 
-void deleteBase(base_t* base);
+void DeleteBase(base_t* base);
 
-void sort(base_t* base);
+void Sort(base_t* base);
 
-bool isValidNumber(telephone_t* telephone);
+bool IsValidNumber(telephone_t* telephone);
 
 
 
--- sources/telephone_info.c
+++ sources/telephone_info.c
@@ -1,1 +1,44 @@
 #include "telephone_info.h"
+
+void CreateBase(base_t *base) {
+    if (base == NULL) {
+        return;
+    }
+    base->capacity_ = 1;
+    base->telephoneCount_ = 0;
+    base->telephones = (telephone_t*)malloc(sizeof(telephone_t));
+}
+
+void Add(base_t *base, unsigned int networkCode,
+         unsigned int number, const char *nameOwner) {
+    if (base->capacity_ == base->telephoneCount_) {
+        base->capacity_ *= 2;
+        base->telephones = (telephone_t*)realloc(base->telephones, base->capacity_ * sizeof(telephone_t));
+    }
+    base->telephones[base->telephoneCount_].networkCode_ = networkCode;
+    base->telephones[base->telephoneCount_].number_ = number;
+
+    if (nameOwner != NULL) {
+        base->telephones[base->telephoneCount_].nameOwner_ = malloc((strlen(nameOwner) + 1));
+        strcpy(base->telephones[base->telephoneCount_].nameOwner_, nameOwner);
+    } else {
+        base->telephones[base->telephoneCount_].nameOwner_ = malloc(sizeof(char));
+        strcpy(base->telephones[base->telephoneCount_].nameOwner_, (const char*)(""));
+    }
+    base->telephoneCount_++;
+}
+
+void DeleteBase(base_t *base) {
+    if (base == NULL) {
+        return;
+    }
+
+    for (unsigned int i = 0; i < base->telephoneCount_; ++i) {
+        if (base->telephones[i].nameOwner_ != NULL) {
+            free(base->telephones[i].nameOwner_);
+        }
+    }
+    free(base->telephones);
+}
+
+
