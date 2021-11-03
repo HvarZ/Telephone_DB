5118f2830ea0ca13d293cfc9b69aa8bfbcba1dfe
HvarZ <hvarzahar@gmail.com>
2021-11-03 13:25:39 +0300

    add implementations of functions: fillBase, Print and validation

--- include/telephone_info.h
+++ include/telephone_info.h
@@ -4,30 +4,34 @@
 #include <stdbool.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdio.h>
 
 typedef struct TelephoneInfo {
-    unsigned int networkCode_;
-    unsigned int number_;
+    unsigned long networkCode_;
+    unsigned long number_;
     char* nameOwner_;
 } telephone_t;
 
 typedef struct TelephoneBase {
-    unsigned int telephoneCount_;
-    unsigned int capacity_;
+    unsigned long telephoneCount_;
+    unsigned long capacity_;
     telephone_t* telephones;
 } base_t;
 
 void CreateBase(base_t* base);
 
-void Add(base_t* base, unsigned int networkCode,
-         unsigned int number, const char* nameOwner);
+void Add(base_t* base, unsigned long networkCode,
+         unsigned long number, const char* nameOwner);
+
+void FillBase(base_t* base);
+
+void Print(base_t* base);
 
 void DeleteBase(base_t* base);
 
 void Sort(base_t* base);
 
-bool IsValidNumber(telephone_t* telephone);
-
+bool IsValidNumber(unsigned long networkCode, unsigned long number);
 
 
 #endif //INDIVIDUAL_TASK_1_TELEPHONE_INFO_H
--- sources/telephone_info.c
+++ sources/telephone_info.c
@@ -1,5 +1,11 @@
 #include "telephone_info.h"
 
+#define MAX_NUMBER_TELEPHONES 100000
+#define MAX_LENGTH_NAME 20
+#define MAX_NETWORK_CODE 9999
+#define MAX_NUMBER 9999999
+#define MIN_NUMBER 1000000
+
 void CreateBase(base_t *base) {
     if (base == NULL) {
         return;
@@ -9,8 +15,11 @@ void CreateBase(base_t *base) {
     base->telephones = (telephone_t*)malloc(sizeof(telephone_t));
 }
 
-void Add(base_t *base, unsigned int networkCode,
-         unsigned int number, const char *nameOwner) {
+void Add(base_t *base, unsigned long networkCode,
+         unsigned long number, const char *nameOwner) {
+    if (base->telephoneCount_ >= MAX_NUMBER_TELEPHONES) {
+        return;
+    }
     if (base->capacity_ == base->telephoneCount_) {
         base->capacity_ *= 2;
         base->telephones = (telephone_t*)realloc(base->telephones, base->capacity_ * sizeof(telephone_t));
@@ -28,17 +37,51 @@ void Add(base_t *base, unsigned int networkCode,
     base->telephoneCount_++;
 }
 
+void FillBase(base_t *base) {
+    if (base == NULL) {
+        return;
+    }
+    unsigned long networkCode;
+    while (scanf("%lu", &networkCode) != EOF) {
+        unsigned long number;
+        scanf("%lu", &number);
+
+        char* nameOwner = malloc(MAX_LENGTH_NAME);
+        scanf("%s", nameOwner);
+        //if (!IsValidNumber(networkCode, number)) {
+        //    continue;
+        //}
+        Add(base, networkCode, number, nameOwner);
+    }
+}
+
 void DeleteBase(base_t *base) {
     if (base == NULL) {
         return;
     }
-
-    for (unsigned int i = 0; i < base->telephoneCount_; ++i) {
+    for (unsigned long i = 0; i < base->telephoneCount_; ++i) {
         if (base->telephones[i].nameOwner_ != NULL) {
             free(base->telephones[i].nameOwner_);
         }
     }
     free(base->telephones);
+}
+
+bool IsValidNumber(unsigned long networkCode, unsigned long number) {
+    return networkCode <= MAX_NETWORK_CODE && (number >= MIN_NUMBER && number <= MAX_NUMBER);
+}
+
+void Print(base_t* base) {
+    if (base == NULL || base->telephoneCount_ == 0) {
+        printf("Base is empty");
+        return;
+    }
+    for (unsigned long i = 0; i < base->telephoneCount_; ++i) {
+        if (base->telephones[i].nameOwner_ != NULL) {
+            printf("+%lu%lu - %s\n", base->telephones[i].networkCode_,
+                   base->telephones[i].number_, base->telephones[i].nameOwner_);
+        }
+    }
 }
 
 
